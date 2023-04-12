849cdd2db1df853742cbf7510a131f2a6d64fedd
EJ Chow <karenchow@EJ-MacBook-Air.local>
2023-04-12 17:12:26 +0200

    testing on firebase credential files

--- SpotifyQuiz/src/app/register/register.component.ts
+++ SpotifyQuiz/src/app/register/register.component.ts
@@ -8,7 +8,11 @@ export class RegisterComponent {
     email : string ="";
     password : string ="";
     show: boolean= false;
+    signup_data = {"username":this.username, "password": this.password, "email": this.email};
     signUp(){
-        
+        let signup_req = new XMLHttpRequest();
+        signup_req.open("POST", "/sign_up/", true);
+        signup_req.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+        signup_req.send(JSON.stringify(this.signup_data));
     }
 }
