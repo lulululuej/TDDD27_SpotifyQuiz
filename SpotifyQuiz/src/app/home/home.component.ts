fa5d05902f84caf76db9097f453e28e955f7e392
marha057 <marha057@student.liu.se>
2023-04-18 11:00:35 +0200

    Adding ability to get Spotify Token
    By using the get Token button on the home-page, a spotify access token
    will be saved to the firebase as well as the local storage.
    
    Firebase code is not properly tested, still need to check if things are
    saved to the firebase or not.

--- SpotifyQuiz/src/app/home/home.component.ts
+++ SpotifyQuiz/src/app/home/home.component.ts
@@ -1,17 +1,25 @@
 import { Component } from "@angular/core";
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { Database, set, ref, onValue } from '@angular/fire/database';
+import { Observable, throwError } from "rxjs";
+import { catchError, retry } from "rxjs/operators";
 
-@Component({ templateUrl: 'home.component.html' })
+
+@Component({ 
+    templateUrl: 'home.component.html' 
+})
 export class HomeComponent {
 
-    constructor(private http : HttpClient) {
-        
-    }
+    constructor(
+        public database : Database,
+        private http : HttpClient
+        ) {};
 
     homeButtonListener(value: any) {
         let btn = document.getElementById("home_spotify_button");
         btn?.addEventListener("click", (e:Event) => this.getSpotifyToken());
     }
+
     getSpotifyToken() {
         let spotifyUrl = "https://accounts.spotify.com/api/token";
         let httpParams = new HttpParams()
@@ -21,11 +29,30 @@ export class HomeComponent {
         let headers = new HttpHeaders({
             'Content-Type':'application/x-www-form-urlencoded'
         });
+        
         let options = {headers:headers};
         const httpObserver = {
             next: localStorage.setItem
         }
-        this.http.post(spotifyUrl, httpParams, options).subscribe();
+        this.http.post(spotifyUrl, httpParams, options).subscribe(
+            response => {
+                let jsonResponse = JSON.parse(JSON.stringify(response))
+                //JSON.parse(response);
+                window.localStorage.setItem("access_token", jsonResponse["access_token"]);
+                window.localStorage.setItem("token_type", jsonResponse["token_type"]);
+                let time = Date.now() + 3600000;
+                let actualTime = new Date();
+                actualTime.setTime(time);
+                window.localStorage.setItem("expires_at", actualTime.toISOString())
+
+                set(ref(this.database, 'spotifyToken'), {
+                    access_token:jsonResponse["access_token"],
+                    token_type:jsonResponse["token_type"],
+                    expiration_time:actualTime.toISOString()
+                })
+                //window.localStorage.setItem("spotifyToken", response)
+            }
+        );
     }
 
 }
